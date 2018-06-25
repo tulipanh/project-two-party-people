3b7567d258f7f8ba0251485382e890f09e35fba3
Jay Feldman <jayfman@gmail.com>
2018-06-25 13:44:54 -0400

    created objects

---
+++ STS/src/main/java/models/Party.java
@@ -0,0 +1,15 @@
+package models;
+
+import java.util.List;
+
+public class Party {
+	
+	int partyId;
+	Address address;
+	String partyName;
+	List<PartyPerson> attendees;
+	String pictureUrl;
+	
+	
+
+}
